6cac8fd2e1dc306b5c930139a57f2f430b84482f
heavenlyren <heavenlyren@gmail.com>
2024-12-10 14:49:28 +0900

    added Pam section

--- joinDomain.sh
+++ joinDomain.sh
@@ -67,6 +67,9 @@ access_provider = ad
 ad_gpo_access_control = disabled
 dyndns_update = True
 ad_hostname = $computer_name
+
+[pam]
+pam_passkey_auth = False
 EOF
 
 #  we set the permissions of sssd.conf
